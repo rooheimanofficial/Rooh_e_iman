04b30e7b2d7d851ab7053b25a0790dd09d965835
Rooh E Iman <rooheiman.official@gmail.com>
2026-08-08 22:03:21 +0500

    Add files via upload

---
+++ paras.js
@@ -0,0 +1,243 @@
+const paras = [
+
+{
+id:1,
+name:"Para 1",
+arabic:"الم",
+start:"Al-Fatihah 1:1",
+end:"Al-Baqarah 2:141"
+},
+
+{
+id:2,
+name:"Para 2",
+arabic:"سَيَقُولُ",
+start:"Al-Baqarah 2:142",
+end:"Al-Baqarah 2:252"
+},
+
+{
+id:3,
+name:"Para 3",
+arabic:"تِلْكَ الرُّسُلُ",
+start:"Al-Baqarah 2:253",
+end:"Aal-Imran 3:92"
+},
+
+{
+id:4,
+name:"Para 4",
+arabic:"لَنْ تَنَالُوا",
+start:"Aal-Imran 3:93",
+end:"An-Nisa 4:23"
+},
+
+{
+id:5,
+name:"Para 5",
+arabic:"وَالْمُحْصَنَاتُ",
+start:"An-Nisa 4:24",
+end:"An-Nisa 4:147"
+},
+
+{
+id:6,
+name:"Para 6",
+arabic:"لَا يُحِبُّ اللَّهُ",
+start:"An-Nisa 4:148",
+end:"Al-Ma'idah 5:81"
+},
+
+{
+id:7,
+name:"Para 7",
+arabic:"وَإِذَا سَمِعُوا",
+start:"Al-Ma'idah 5:82",
+end:"Al-An'am 6:110"
+},
+
+{
+id:8,
+name:"Para 8",
+arabic:"وَلَوْ أَنَّنَا",
+start:"Al-An'am 6:111",
+end:"Al-A'raf 7:87"
+},
+
+{
+id:9,
+name:"Para 9",
+arabic:"قَالَ الْمَلَأُ",
+start:"Al-A'raf 7:88",
+end:"Al-Anfal 8:40"
+},
+
+{
+id:10,
+name:"Para 10",
+arabic:"وَاعْلَمُوا",
+start:"Al-Anfal 8:41",
+end:"At-Tawbah 9:92"
+},
+
+{
+id:11,
+name:"Para 11",
+arabic:"يَعْتَذِرُونَ",
+start:"At-Tawbah 9:93",
+end:"Hud 11:5"
+},
+
+{
+id:12,
+name:"Para 12",
+arabic:"وَمَا مِنْ دَابَّةٍ",
+start:"Hud 11:6",
+end:"Yusuf 12:52"
+},
+
+{
+id:13,
+name:"Para 13",
+arabic:"وَمَا أُبَرِّئُ",
+start:"Yusuf 12:53",
+end:"Ibrahim 14:52"
+},
+
+{
+id:14,
+name:"Para 14",
+arabic:"رُبَمَا",
+start:"Al-Hijr 15:1",
+end:"An-Nahl 16:128"
+},
+
+{
+id:15,
+name:"Para 15",
+arabic:"سُبْحَانَ الَّذِي",
+start:"Al-Isra 17:1",
+end:"Al-Kahf 18:74"
+},
+
+{
+id:16,
+name:"Para 16",
+arabic:"قَالَ أَلَمْ",
+start:"Al-Kahf 18:75",
+end:"Ta-Ha 20:135"
+},
+
+{
+id:17,
+name:"Para 17",
+arabic:"اقْتَرَبَ",
+start:"Al-Anbiya 21:1",
+end:"Al-Hajj 22:78"
+},
+
+{
+id:18,
+name:"Para 18",
+arabic:"قَدْ أَفْلَحَ",
+start:"Al-Mu'minun 23:1",
+end:"Al-Furqan 25:20"
+},
+
+{
+id:19,
+name:"Para 19",
+arabic:"وَقَالَ الَّذِينَ",
+start:"Al-Furqan 25:21",
+end:"An-Naml 27:55"
+},
+
+{
+id:20,
+name:"Para 20",
+arabic:"أَمَّنْ خَلَقَ",
+start:"An-Naml 27:56",
+end:"Al-Ankabut 29:45"
+},
+
+{
+id:21,
+name:"Para 21",
+arabic:"اتْلُ مَا أُوحِيَ",
+start:"Al-Ankabut 29:46",
+end:"Al-Ahzab 33:30"
+},
+
+{
+id:22,
+name:"Para 22",
+arabic:"وَمَنْ يَقْنُتْ",
+start:"Al-Ahzab 33:31",
+end:"Ya-Sin 36:27"
+},
+
+{
+id:23,
+name:"Para 23",
+arabic:"وَمَا لِيَ",
+start:"Ya-Sin 36:28",
+end:"Az-Zumar 39:31"
+},
+
+{
+id:24,
+name:"Para 24",
+arabic:"فَمَنْ أَظْلَمُ",
+start:"Az-Zumar 39:32",
+end:"Fussilat 41:46"
+},
+
+{
+id:25,
+name:"Para 25",
+arabic:"إِلَيْهِ يُرَدُّ",
+start:"Fussilat 41:47",
+end:"Al-Jathiyah 45:37"
+},
+
+{
+id:26,
+name:"Para 26",
+arabic:"حم",
+start:"Al-Ahqaf 46:1",
+end:"Adh-Dhariyat 51:30"
+},
+
+{
+id:27,
+name:"Para 27",
+arabic:"قَالَ فَمَا خَطْبُكُمْ",
+start:"Adh-Dhariyat 51:31",
+end:"Al-Hadid 57:29"
+},
+
+{
+id:28,
+name:"Para 28",
+arabic:"قَدْ سَمِعَ اللَّهُ",
+start:"Al-Mujadilah 58:1",
+end:"At-Tahrim 66:12"
+},
+
+{
+id:29,
+name:"Para 29",
+arabic:"تَبَارَكَ الَّذِي",
+start:"Al-Mulk 67:1",
+end:"Al-Mursalat 77:50"
+},
+
+{
+id:30,
+name:"Para 30",
+arabic:"عَمَّ",
+start:"An-Naba 78:1",
+end:"An-Nas 114:6"
+},
+
+];
